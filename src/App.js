52da4394c54c1244f1ec44f25f13d08164e09061
Aibek <aibekzhv@gmail.com>
2019-07-09 10:17:24 -0700

    mounts router to app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {
+    Route,
+    Link,
+    Switch,
+    Redirect,
+    BrowserRouter as Router
+} from 'react-router-dom';
 
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Here's my components
+
+
+function Home() {
+    return (
+        <div>
+          <h1 >Welcome to React Firebase Todos</h1>
+        </div>
+    );
 }
 
+function Dashboard() {
+
+    return (
+        <div>
+          <h1 >Welcome to Dashboard</h1>
+        </div>
+    );
+}
+function Login() {
+
+    return (
+      <div>
+        <h1 >You need to be logged in to see this page</h1>
+        <button>Login with Google </button>
+      </div>
+    )
+}
+
+class App extends Component {
+  render() { 
+    return ( 
+      <Router>    
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+        <Switch> 
+
+          <Route exact path="/" component={Home}/>
+          <Route path="/dashboard" component={Dashboard}/>
+          <Route path="/login" component={Login}/>
+
+        </Switch>
+
+      </Router>
+     );
+  }
+}
+ 
 export default App;
