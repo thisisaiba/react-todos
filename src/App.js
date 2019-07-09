b25b1a5c68f3a98f3c1f9a713f0b4a48afb0e79a
Aibek <aibekzhv@gmail.com>
2019-07-09 13:45:20 -0700

    comoletes user story 7

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+
 import {
     Route,
     Link,
@@ -7,59 +8,176 @@ import {
     BrowserRouter as Router
 } from 'react-router-dom';
 
+import {
+    login,
+    logout,
+    auth,
+    createTodo
+} from './utils/firebaseService';
 
-//Here's my components
 
+const linkStyle = {
+    textDecoration: 'underline',
+    color: 'rebeccapurple',
+    cursor: 'pointer'
+};
 
-function Home() {
+// Here's our Private Route
+
+function PrivateRoute({ authenticated, component: Component, ...rest }) {
     return (
-        <div>
-          <h1 >Welcome to React Firebase Todos</h1>
-        </div>
-    );
-}
-
-function Dashboard() {
-
-    return (
-        <div>
-          <h1 >Welcome to Dashboard</h1>
-        </div>
-    );
-}
-function Login() {
-
-    return (
-      <div>
-        <h1 >You need to be logged in to see this page</h1>
-        <button>Login with Google </button>
-      </div>
+        <Route render={props => (
+            authenticated
+            ? <Component {...rest} {...props} />
+            : <Redirect to="/login" />
+        )}/>
     )
 }
 
-class App extends Component {
-  render() { 
-    return ( 
-      <Router>    
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-        </ul>
-        <Switch> 
 
-          <Route exact path="/" component={Home}/>
-          <Route path="/dashboard" component={Dashboard}/>
-          <Route path="/login" component={Login}/>
 
-        </Switch>
+// Here's our child components
 
-      </Router>
-     );
-  }
+function Home() {
+    return(
+        <div>
+            <h1>Welcome to React Firebase Todos</h1>
+        </div>
+    )
 }
- 
+
+function Dashboard({
+    user,
+    text,
+    handleChange,
+    handleSubmit
+  
+  }) {
+    return(
+        <div>
+            <h2>Welcome to your Dashboard, {user.displayName.split(" ")[0]}</h2>
+            <img
+            style={{
+                height: 100,
+                borderRadius: '50%',
+                border: '2px solid black'
+            }} 
+            src={user.photoURL} 
+            alt={user.displayName}
+            />
+            <hr />
+            <h5>Here's your todo items</h5>
+            <ul>
+              {
+                /* we'll map through out todo items here */
+              }
+            </ul>
+            <form onSubmit={handleSubmit}>
+                <input name="text" value={text} onChange={handleChange}></input>
+                <button>Add Todo</button>
+            </form>
+        </div>
+    )
+}
+
+function Login({ authenticated }) {
+    if(authenticated) return <Redirect to="/dashboard" />
+    return(
+        <div>
+            <h2>You need to be logged in to see this page</h2>
+            <button onClick={login}>Login With Google</button>
+        </div>
+    )
+}
+
+
+
+// Here's our Parent Component
+class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            authenticated: false,
+            user: null,
+            text: " ",
+            dbRef: null
+        }
+    }
+
+    handleChange = e => {
+      this.setState({[e.target.name]: e.target.value});
+    };
+
+    handleSubmit = e => {
+      const {dbRef, text} = this.state;
+      e.preventDefault();
+      createTodo(dbRef, {
+        text,
+        completed: false
+      }).then(() => this.setState({ text: ""}));
+      //Call create todo passing in two args
+      //1 Ref to the DB point for todo items
+      //2) todo item itself - text, - str, comleted - bool
+    }
+
+    componentDidMount() {
+        auth.onAuthStateChanged(user => {
+            if(user) {
+                this.setState({
+                    authenticated: true,
+                    user,
+                    dbRef: `users/${user.uid}/todos`
+                });
+            } else {
+                this.setState({ 
+                    authenticated: false,
+                    user: null,
+                    dbRef: null
+                });
+            }
+        })
+    }
+
+    render() {
+        return (
+            <Router>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/dashboard">Dashboard</Link>
+                    </li>
+                    {
+                        this.state.authenticated
+                        &&
+                        <li style={linkStyle}>
+                            <span onClick={logout}>Logout</span>
+                        </li>
+                    }
+                </ul>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <PrivateRoute 
+                      authenticated={this.state.authenticated}
+                      handleChange={this.handleChange}
+                      handleSubmit={this.handleSubmit}
+                      user={this.state.user}
+                      text={this.state.text}
+                      path="/dashboard" 
+                      component={Dashboard} 
+                    />
+                    <Route path="/login" render={props => (
+                        <Login 
+                        {...props}
+                        authenticated={this.state.authenticated}
+                        />
+                    )} />
+                </Switch>
+            </Router>
+        );
+    }
+}
+
 export default App;
---
+++ src/utils/firebaseService.js
@@ -0,0 +1,43 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
+
+firebase.initializeApp({
+    apiKey:process.env.REACT_APP_API_KEY,
+    authDomain:process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL:process.env.REACT_APP_DATABASE_URL,
+    projectId:process.env.REACT_APP_PROJECT_ID,
+    storageBucket: "",
+    messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId:process.env.REACT_APP_APP_ID
+});
+
+// Apps and config vars
+const provider = new firebase.auth.GoogleAuthProvider();
+const database = firebase.database();
+const auth = firebase.auth()
+
+//functions
+
+
+
+function login() {
+    return auth.signInWithPopup(provider);
+}
+
+function logout() {
+    return auth.signOut();
+}
+
+function createTodo(ref, todo) {
+   return  database.ref(ref).push(todo);
+
+} 
+
+export {
+    login,
+    logout,
+    auth,
+    database,
+    createTodo
+}
